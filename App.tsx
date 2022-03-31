29bbed6df34bcf78e20717ce58b25a38c2f956ea
Eric Samuelsson <gossen02@gmail.com>
2022-03-31 10:46:39 +0100

    Finished exercises for kmom01

---
+++ App.tsx
@@ -0,0 +1,30 @@
+import { StatusBar } from 'expo-status-bar';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import warehouse from './assets/warehouse.jpg';
+import Stock from './components/Stock.tsx';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.base}>
+        <Text style={{ color: '#ff0000', fontSize: 42 }}>Välkommen till Lager-Appen</Text>
+        <Image source={warehouse} style={{ width: 320, height: 240 }} />
+        <Stock />
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  base: {
+    flex: 1,
+    backgroundColor: '#ffe4c4',
+    paddingLeft: 12,
+    paddingRight: 12,
+  }
+});
