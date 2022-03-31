fd2eb1105e1cfeebe78dc8152adad51c17048755
Eric Samuelsson <gossen02@gmail.com>
2022-03-31 11:10:55 +0100

    Changed to ScrollView

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, ScrollView } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import warehouse from './assets/warehouse.jpg';
 import Stock from './components/Stock.tsx';
@@ -7,12 +7,12 @@ import Stock from './components/Stock.tsx';
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.base}>
+      <ScrollView style={styles.base}>
         <Text style={{ color: '#ff0000', fontSize: 42 }}>Välkommen till Lager-Appen</Text>
         <Image source={warehouse} style={{ width: 320, height: 240 }} />
         <Stock />
         <StatusBar style="auto" />
-      </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
