db412090cba044fdb33686c9079f21992c2a1a3d
Eric Samuelsson <gossen02@gmail.com>
2022-04-08 10:22:10 +0100

    New structure for style

---
+++ styles/base.js
@@ -0,0 +1,16 @@
+export const container = {
+    flex: 1,
+}
+
+export const base = {
+    flex: 1,
+    backgroundColor: '#ffe4c4',
+    paddingLeft: 12,
+    paddingRight: 12,
+};
+
+export const warehouseimg = {
+    width: 320,
+    height: 240,
+    marginBottom: 28,
+}
