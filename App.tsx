01684c78a04931141eb7cfcbc088f7b60c8c1973
Eric Samuelsson <gossen02@gmail.com>
2022-04-25 10:01:30 +0100

    Add new components Invoice & Login

--- App.tsx
+++ App.tsx
@@ -1,25 +1,35 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons';
 
-import Home from "./components/Home.tsx";
-import Pick from "./components/Pick.tsx";
+import Home from "./components/Home";
+import Pick from "./components/Pick";
 import Deliveries from "./components/Deliveries";
+import Auth from "./components/auth/Auth";
+import Invoices from "./components/invoice/Invoices";
+import authModel from "./models/auth";
 import { Base } from "./styles/index.js";
 
 const routeIcons = {
   "Lager": "home",
   "Plock": "list",
   "Inleveranser": "car-outline",
+  "Faktura": "cash-outline",
+  "Logga in": "lock-closed",
 };
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   const [products, setProducts] = useState([]);
+  const [isLoggedIn, setIsLoggedIn] = useState<Boolean>(false);
+
+  useEffect(async () => {
+    setIsLoggedIn(await authModel.loggedIn());
+  }, []);
 
   return (
     <SafeAreaView style={Base.container}>
@@ -43,6 +53,14 @@ export default function App() {
           <Tab.Screen name="Inleveranser">
             {() => <Deliveries setProducts={setProducts}/>}
           </Tab.Screen>
+          {isLoggedIn ?
+            <Tab.Screen name="Faktura">
+              {() => <Invoices setIsLoggedIn={setIsLoggedIn}/>}
+            </Tab.Screen> :
+            <Tab.Screen name="Logga in">
+              {() => <Auth setIsLoggedIn={setIsLoggedIn}/>}
+            </Tab.Screen>
+          }
         </Tab.Navigator>
       </NavigationContainer>
       <StatusBar style="auto" />
